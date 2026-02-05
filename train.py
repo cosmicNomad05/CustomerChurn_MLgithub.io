95c43d52a8bbc7cf91a88436570770ee498b9fd4
cosmicNomad05 <veekshavn@gmail.com>
2026-02-05 21:35:41 +0530

    Selected logistic regression as final model with scaling

--- train.py
+++ train.py
@@ -2,10 +2,11 @@ import pandas as pd
 import pickle
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+
 
 #1. Load Cleaned Dataset
 df = pd.read_csv("data/telco.csv")
@@ -28,7 +29,7 @@ y = df["Churn Label"]
 
 #3. Encode Categorical Features
 label_encoders = {}
-for col in X.select_dtypes(include="object").columns:
+for col in X.select_dtypes(include=["object", "string"]).columns:
     le = LabelEncoder()
     X[col] = le.fit_transform(X[col])
     label_encoders[col] = le
@@ -43,9 +44,13 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 #5. Train Models
 # Logistic Regression
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
 lr = LogisticRegression(max_iter=1000)
-lr.fit(X_train, y_train)
-y_pred_lr = lr.predict(X_test)
+lr.fit(X_train_scaled, y_train)
+y_pred_lr = lr.predict(X_test_scaled)
 
 print("Logistic Regression Accuracy:",
       accuracy_score(y_test, y_pred_lr))
@@ -69,7 +74,10 @@ print(classification_report(y_test, y_pred_rf))
 
 #7. Save Best Model
 with open("model/churn_model.pkl", "wb") as f:
-    pickle.dump(rf, f)
+    pickle.dump(lr, f)
 
 with open("model/encoders.pkl", "wb") as f:
     pickle.dump(label_encoders, f)
+
+with open("model/scaler.pkl", "wb") as f:
+    pickle.dump(scaler, f)
