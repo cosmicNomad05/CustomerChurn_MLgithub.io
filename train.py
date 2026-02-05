2974c5b27e6c3fd0f117c14022a050729636c765
cosmicNomad05 <veekshavn@gmail.com>
2026-02-05 21:26:04 +0530

    Fixed pandas warning and finalized model selection

---
+++ train.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import pickle
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+
+#1. Load Cleaned Dataset
+df = pd.read_csv("data/telco.csv")
+df.columns = df.columns.str.strip()
+# Drop leakage columns
+leakage_cols = [
+    "Customer ID",
+    "Customer Status",
+    "Churn Category",
+    "Churn Reason",
+    "Churn Score"
+]
+df = df.drop(columns=leakage_cols)
+# Drop missing values
+df = df.dropna()
+
+#2. Separate Features & Target
+X = df.drop("Churn Label", axis=1)
+y = df["Churn Label"]
+
+#3. Encode Categorical Features
+label_encoders = {}
+for col in X.select_dtypes(include="object").columns:
+    le = LabelEncoder()
+    X[col] = le.fit_transform(X[col])
+    label_encoders[col] = le
+
+#4. Train-Test Split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
+
+#5. Train Models
+# Logistic Regression
+lr = LogisticRegression(max_iter=1000)
+lr.fit(X_train, y_train)
+y_pred_lr = lr.predict(X_test)
+
+print("Logistic Regression Accuracy:",
+      accuracy_score(y_test, y_pred_lr))
+
+# Random Forest
+rf = RandomForestClassifier(
+    n_estimators=200,
+    random_state=42
+)
+rf.fit(X_train, y_train)
+y_pred_rf = rf.predict(X_test)
+print("Random Forest Accuracy:",
+      accuracy_score(y_test, y_pred_rf))
+
+#6. Evaluation
+print("\nLogistic Regression Report\n")
+print(classification_report(y_test, y_pred_lr))
+
+print("\nRandom Forest Report\n")
+print(classification_report(y_test, y_pred_rf))
+
+#7. Save Best Model
+with open("model/churn_model.pkl", "wb") as f:
+    pickle.dump(rf, f)
+
+with open("model/encoders.pkl", "wb") as f:
+    pickle.dump(label_encoders, f)
